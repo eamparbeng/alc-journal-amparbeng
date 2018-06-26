583902694d1096824369de4b416b7b70f7366ee9
Kofi Amparbeng <kofi@acsghana.com>
2018-06-26 10:01:07 +0000

    Pull to Refresh + Cancel Button on New Entry Form

--- app/src/main/java/alc/kofiamparbeng/ampjournal/activities/MainActivity.java
+++ app/src/main/java/alc/kofiamparbeng/ampjournal/activities/MainActivity.java
@@ -4,11 +4,14 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Toast;
 
@@ -25,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private JournalEntryViewModel mJournalEntryViewModel;
 
+    private SwipeRefreshLayout swipeContainer;
+
     public static final int NEW_JOURNAL_ENTRY_ACTIVITY_REQUEST_CODE = 712;
 
     @Override
@@ -35,9 +40,36 @@ public class MainActivity extends AppCompatActivity {
         prepareFloatingActionButton();
         prepareRecyclerView();
         prepareViewModel();
+
+        preparePullToRefresh();
     }
 
-    private void prepareViewModel(){
+    private void preparePullToRefresh(){
+        swipeContainer = (SwipeRefreshLayout)findViewById(R.id.swipeContainer);
+        swipeContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               prepareViewModel();
+                swipeContainer.setRefreshing(false);
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(R.color.colorAccent,
+                R.color.colorPrimary,
+                android.R.color.holo_orange_light,
+                R.color.colorPrimaryDark);
+
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                prepareViewModel();
+                swipeContainer.setRefreshing(false);
+            }
+        });
+    }
+
+    private void prepareViewModel() {
         mJournalEntryViewModel = ViewModelProviders.of(this).get(JournalEntryViewModel.class);
         mJournalEntryViewModel.getAllEntries().observe(this, new Observer<List<JournalEntry>>() {
             @Override
@@ -83,8 +115,14 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(
                     getApplicationContext(),
-                    R.string.new_journal_entry_invalid_message,
+                    R.string.new_journal_entry_cancelled_message,
                     Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
--- app/src/main/java/alc/kofiamparbeng/ampjournal/activities/NewJournalEntryActivity.java
+++ app/src/main/java/alc/kofiamparbeng/ampjournal/activities/NewJournalEntryActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -58,4 +60,20 @@ public class NewJournalEntryActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_new_journal_entry, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.cancel_new_journal_form) {
+            Intent replyIntent = new Intent();
+            setResult(RESULT_CANCELED, replyIntent);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
